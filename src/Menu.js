b258626c8d699d76258217bc8bd7396809e4c457
bichoogang <66268274+bichoogang@users.noreply.github.com>
2021-06-28 02:02:43 +0530

    Update Menu.js

--- src/Menu.js
+++ src/Menu.js
@@ -10,7 +10,7 @@ function Menu() {
                     
               
         <nav className="navbar navbar-expand-lg navbar-light ">
-        <NavLink className="navbar-brand" exact  to="/">WebWala</NavLink>
+        <NavLink className="navbar-brand" exact  to="/">Logo</NavLink>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
